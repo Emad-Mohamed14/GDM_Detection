af173ba7e34f91480f7e8e5310a84b275ddfed74
Emad Mohamed <emad.ka.03@gmail.com>
2024-06-18 22:11:36 +0530

    HyperParameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,13 +44,87 @@ class ModelTrainer:
                 "Random Forest": RandomForestClassifier(),
                 "Decision Tree": DecisionTreeClassifier(),
                 "Gradient Boosting": GradientBoostingClassifier(),
-                "K-Neighbors Classifier": KNeighborsClassifier(),
                 "XGBClassifier": XGBClassifier(),
                 "CatBoosting Classifier": CatBoostClassifier(verbose=False),
                 "AdaBoost Classifier": AdaBoostClassifier(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            '''For Regression:-
+            params={
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            '''
+
+            '''For Classification'''
+            params={
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'criterion': ['gini', 'entropy'],
+                    'max_features': ['sqrt', 'log2', None]
+                },
+                "Decision Tree": {
+                    'criterion': ['gini', 'entropy'],
+                    'splitter': ['best', 'random'],
+                    'max_features': ['sqrt', 'log2', None]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'criterion': ['friedman_mse'],
+                    'max_features': ['sqrt', 'log2'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "XGBClassifier": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'max_depth': [3, 4, 5, 6],
+                    'gamma': [0, 0.1, 0.2, 0.3],
+                    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0]
+                },
+                "CatBoosting Classifier": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             #For getting best model score from dict
             best_model_score = max(sorted(model_report.values()))
